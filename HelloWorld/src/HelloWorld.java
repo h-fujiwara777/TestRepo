27642e8a73cb4d6a12e02fc8d275bdf1e6fbcc09
h-fujiwara <h-fujiwara@DESKTOP-05BDPB2>
2020-06-05 09:55:50 +0900

    testコミット

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+
+	private int x;
+	public HelloWorld(int i) {
+		x = i;
+		System.out.println("int:" + x);
+	}
+
+	public HelloWorld() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
+
+
+
